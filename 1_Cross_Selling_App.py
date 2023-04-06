318a64b4677737f33ef66a92a824c7f16b10549d
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-04-06 05:06:53 +0200

    Add files via upload

---
+++ 1_Cross_Selling_App.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.write("# Welcome to the Insurance Cross-Sell Prediction Web App! 👋")
+
+st.markdown("""
+        **👈 Select a page from the dropdown on the left** to see some applications of the app!"""
+        )
