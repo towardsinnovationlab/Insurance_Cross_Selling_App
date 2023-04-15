8f5914931c409136c3ccf7f0d0c08dc87a8e9388
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-04-15 14:04:51 +0200

    Update 7_Spot_Check_Models.py

--- pages/7_Spot_Check_Models.py
+++ pages/7_Spot_Check_Models.py
@@ -40,42 +40,54 @@ y_train = pd.read_csv(DATA_URL_ytr)
 DATA_URL_yte = ('./data/y_test.csv')
 y_test = pd.read_csv(DATA_URL_yte)
 
-# LR Calibration models
-#skf = StratifiedKFold(n_splits=5,random_state=0, shuffle=True)
-#LR_C= CalibratedClassifierCV(base_estimator=LogisticRegression(random_state=0), method="isotonic")
-#param_grid = {}
-#model = GridSearchCV(LR_C,param_grid,cv=skf)
-#classifier = model.fit(X_train, y_train)
-# save the model to disk
-#LR_filename = ('./data/LR_C_model.sav')
-#joblib.dump(LR_C_model, LR_filename)
-# load the model from disk
-#LR_C_restored_model = joblib.load(LR_filename)
-#pickle_out = open('./data/classifier.pkl', 'wb')
-#serialized_classifier = pickle.dumps(classifier, pickle_out)
-#pickle_out.write(serialized_classifier)
-#pickle_out.close()
+# LR model
 # loading in the model to predict on the data
-#pickle_in = open('./data/classifier.pkl', 'rb')
-#classifier = pickle.loads(pickle_in)
-#pickle_in.close()
-
-# save the model to disk
-#with open('./data/classifier.pkl', 'wb') as pickle_out:
-#    serialized_classifier = pickle.dumps(classifier)
-#    pickle_out.write(serialized_classifier)
-
-# loading in the model to predict on the data
-with open('./data/classifier.pkl', 'rb') as pickle_in:
+with open('./data/LR_classifier.pkl', 'rb') as pickle_in:
     classifier = pickle.load(pickle_in)
     
-predictions_tr = classifier.predict_proba(X_train)[:, 1]
-predictions_t = classifier.predict_proba(X_test)[:, 1]
+predictions_tr = LR_classifier.predict_proba(X_train)[:, 1]
+predictions_t = LR_classifier.predict_proba(X_test)[:, 1]
 LR_auc_train = roc_auc_score(y_train, predictions_tr)  
 LR_auc_test = roc_auc_score(y_test, predictions_t) 
 score= {'model_c':['LR_C'], 'auc_train_c':[LR_auc_train],'auc_test_c':[LR_auc_test]}
 LR_score= pd.DataFrame(score)
-LR_score
+
+# LR model
+# loading in the model to predict on the data
+with open('./data/LR_classifier.pkl', 'rb') as pickle_in:
+    classifier = pickle.load(pickle_in)
+    
+predictions_tr = LR_classifier.predict_proba(X_train)[:, 1]
+predictions_t = LR_classifier.predict_proba(X_test)[:, 1]
+LR_auc_train = roc_auc_score(y_train, predictions_tr)  
+LR_auc_test = roc_auc_score(y_test, predictions_t) 
+score= {'model_c':['LR_C'], 'auc_train_c':[LR_auc_train],'auc_test_c':[LR_auc_test]}
+LR_score= pd.DataFrame(score)
+
+# LR model
+# loading in the model to predict on the data
+with open('./data/LR_classifier.pkl', 'rb') as pickle_in:
+    classifier = pickle.load(pickle_in)
+    
+predictions_tr = LR_classifier.predict_proba(X_train)[:, 1]
+predictions_t = LR_classifier.predict_proba(X_test)[:, 1]
+LR_auc_train = roc_auc_score(y_train, predictions_tr)  
+LR_auc_test = roc_auc_score(y_test, predictions_t) 
+score= {'model_c':['LR_C'], 'auc_train_c':[LR_auc_train],'auc_test_c':[LR_auc_test]}
+LR_score= pd.DataFrame(score)
+
+# LR model
+# loading in the model to predict on the data
+with open('./data/LR_classifier.pkl', 'rb') as pickle_in:
+    classifier = pickle.load(pickle_in)
+    
+predictions_tr = LR_classifier.predict_proba(X_train)[:, 1]
+predictions_t = LR_classifier.predict_proba(X_test)[:, 1]
+LR_auc_train = roc_auc_score(y_train, predictions_tr)  
+LR_auc_test = roc_auc_score(y_test, predictions_t) 
+score= {'model_c':['LR_C'], 'auc_train_c':[LR_auc_train],'auc_test_c':[LR_auc_test]}
+LR_score= pd.DataFrame(score)
+
 
 st.markdown("""
 Logistic Regression (LR) is the benchmark model used in Insurance, and it has been compared with
