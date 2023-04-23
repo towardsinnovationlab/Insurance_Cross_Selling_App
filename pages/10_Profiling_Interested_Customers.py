776bd52176dcb22dc845aa810e75644918007780
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-04-23 11:47:32 +0200

    Update 10_Profiling_Interested_Customers.py

--- pages/10_Profiling_Interested_Customers.py
+++ pages/10_Profiling_Interested_Customers.py
@@ -21,7 +21,7 @@ from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
-
+import pickle5 as pickle
 
 import warnings
 warnings.simplefilter(action='ignore', category=UserWarning)
@@ -38,6 +38,7 @@ df['Region_Code'] = df['Region_Code'].astype('object')
 df['Previously_Insured'] = df['Previously_Insured'].astype('object')
 df['Policy_Sales_Channel'] = df['Policy_Sales_Channel'].astype('object')
 df['Response'] = df['Response'].astype('object')
+df.drop(['id'], axis=1, inplace=True)
 
 numerical_cols = [var for var in df.columns if df[var].dtype in ['float64','int64']]
 
@@ -47,5 +48,24 @@ df_1 = df_1[df_1['Response']==1]
 
 # Select numerical columns
 num_1 = df_1[numerical_cols]
-num_1
+
+kmeans = pickle.load('./data/kmeans.pkl')
+
+labels = kmeans.predict(num_sc)
+cluster_num = num_1.copy()
+cluster_num['kmeans_cluster'] = labels
+
+df_cluster = pd.concat([df_1, cluster_num['kmeans_cluster']], axis=1)
+
+# Group clusters by Annual Premium
+df = df_cluster.groupby(df_cluster['kmeans_cluster'], as_index=False)['Annual_Premium'].sum()
+df['PERCENTAGE'] = df['Annual_Premium']/df['Annual_Premium'].sum()*100
+# dropping not matching rows
+df = df.dropna()
+# ranking 
+df = df.sort_values(by = 'Annual_Premium', ascending = False).reset_index(drop=True)
+df_AP = df.style.background_gradient(cmap='winter').format({'PERCENTAGE': "{:.2f}"}).format({'Annual_Premium':"{:,.2f}"})
+df_AP
+
+
 
