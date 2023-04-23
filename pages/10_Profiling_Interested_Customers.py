fb170794afae28c77a3204a691e7b6003863c81c
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-04-23 07:01:53 +0200

    Update 10_Profiling_Interested_Customers.py

--- pages/10_Profiling_Interested_Customers.py
+++ pages/10_Profiling_Interested_Customers.py
@@ -53,72 +53,72 @@ sc = StandardScaler()
 num_sc = sc.fit_transform(num_1)
 
 # loading in the model to predict on the data
-with open('./data/kmeans.pkl', 'rb') as pickle_in:
-    kmeans = pickle.load(pickle_in)
+#with open('./data/kmeans.pkl', 'rb') as pickle_in:
+#    kmeans = pickle.load(pickle_in)
 
-#kmeans = KMeans(n_clusters=4, random_state=0).fit(num_sc)
-#labels = kmeans.predict(num_sc)
-#labels
-#cluster_num = num_1.copy()
-#cluster_num['kmeans_cluster'] = labels
+kmeans = KMeans(n_clusters=4, random_state=0).fit(num_sc)
+labels = kmeans.predict(num_sc)
+labels
+cluster_num = num_1.copy()
+cluster_num['kmeans_cluster'] = labels
 len(np.unique(kmeans.labels_))
 
-#cluster = cluster_num['kmeans_cluster'].value_counts()
+cluster = cluster_num['kmeans_cluster'].value_counts()
 
-#df_cluster = pd.concat([df_1, cluster_num['kmeans_cluster']], axis=1)
+df_cluster = pd.concat([df_1, cluster_num['kmeans_cluster']], axis=1)
 
 st.subheader('Group clusters by Annual Premium')
 # Group clusters by Annual Premium
-#df = df_cluster.groupby(df_cluster['kmeans_cluster'], as_index=False)['Annual_Premium'].sum()
-#df['PERCENTAGE'] = df['Annual_Premium']/df['Annual_Premium'].sum()*100
+df = df_cluster.groupby(df_cluster['kmeans_cluster'], as_index=False)['Annual_Premium'].sum()
+df['PERCENTAGE'] = df['Annual_Premium']/df['Annual_Premium'].sum()*100
 # dropping not matching rows
-#df = df.dropna()
+df = df.dropna()
 # ranking 
-#df = df.sort_values(by = 'Annual_Premium', ascending = False).reset_index(drop=True)
-#df_AP = df.style.background_gradient(cmap='winter').format({'PERCENTAGE': "{:.2f}"}).format({'Annual_Premium':"{:,.2f}"})
-#df_AP
+df = df.sort_values(by = 'Annual_Premium', ascending = False).reset_index(drop=True)
+df_AP = df.style.background_gradient(cmap='winter').format({'PERCENTAGE': "{:.2f}"}).format({'Annual_Premium':"{:,.2f}"})
+df_AP
 
 st.subheader('Group clusters by Age')
 # Group clusters by Age
-#df = df_cluster.groupby(df_cluster['kmeans_cluster'], as_index=False)['Age'].mean()
-#df['PERCENTAGE'] = df['Age']/df['Age'].sum()*100
+df = df_cluster.groupby(df_cluster['kmeans_cluster'], as_index=False)['Age'].mean()
+df['PERCENTAGE'] = df['Age']/df['Age'].sum()*100
 # dropping not matching rows
-#df = df.dropna()
+df = df.dropna()
 # ranking 
-#df = df.sort_values(by = 'Age', ascending = False).reset_index(drop=True)
-#df_AGE = df.style.background_gradient(cmap='winter').format({'PERCENTAGE': "{:.2f}"}).format({'Age':"{:,.2f}"})
-#df_AGE
+df = df.sort_values(by = 'Age', ascending = False).reset_index(drop=True)
+df_AGE = df.style.background_gradient(cmap='winter').format({'PERCENTAGE': "{:.2f}"}).format({'Age':"{:,.2f}"})
+df_AGE
 
 # Select top cluster per Annual Premium and Age
-#df_cluster_AP = df_cluster[df_cluster['kmeans_cluster']==2].reset_index(drop=True)
-#df_cluster_AGE = df_cluster[df_cluster['kmeans_cluster']==1].reset_index(drop=True)
+df_cluster_AP = df_cluster[df_cluster['kmeans_cluster']==2].reset_index(drop=True)
+df_cluster_AGE = df_cluster[df_cluster['kmeans_cluster']==1].reset_index(drop=True)
 
 st.subheader('Annual Premium distribution vs Gender, Vehicle_Damage, Vehicle_Age for the Top Cluster')
 # Plot Annual Premium vs Gender, Vehicle_Damage, Vehicle_Age
-#fig = plt.figure()
-#plt.rcParams['figure.figsize']=(20,7)
-#plt.subplot(1,3,1)
-#sns.kdeplot(x=df_cluster_AP['Annual_Premium'],hue=df_cluster_AP['Gender'],palette="crest", multiple='stack')
-#plt.title('Annual_Premium vs Gender on Top Cluster')
-#plt.subplot(1,3,2)
-#sns.kdeplot(x=df_cluster_AP['Annual_Premium'],hue=df_cluster_AP['Vehicle_Damage'],palette="crest", multiple='stack')
-#plt.title('Annual_Premium vs Vehicle_Damage on Top Cluster')
-#plt.subplot(1,3,3)
-#sns.kdeplot(x=df_cluster_AP['Annual_Premium'],hue=df_cluster_AP['Vehicle_Age'],palette="crest", multiple='stack')
-#plt.title('Annual_Premium vs Vehicle_Age on Top Cluster')
-#st.pyplot(fig)
+fig = plt.figure()
+plt.rcParams['figure.figsize']=(20,7)
+plt.subplot(1,3,1)
+sns.kdeplot(x=df_cluster_AP['Annual_Premium'],hue=df_cluster_AP['Gender'],palette="crest", multiple='stack')
+plt.title('Annual_Premium vs Gender on Top Cluster')
+plt.subplot(1,3,2)
+sns.kdeplot(x=df_cluster_AP['Annual_Premium'],hue=df_cluster_AP['Vehicle_Damage'],palette="crest", multiple='stack')
+plt.title('Annual_Premium vs Vehicle_Damage on Top Cluster')
+plt.subplot(1,3,3)
+sns.kdeplot(x=df_cluster_AP['Annual_Premium'],hue=df_cluster_AP['Vehicle_Age'],palette="crest", multiple='stack')
+plt.title('Annual_Premium vs Vehicle_Age on Top Cluster')
+st.pyplot(fig)
 
 st.subheader('Age distribution vs Gender, Vehicle_Damage, Vehicle_Age for the Top Cluster')
 # Plot Age vs Gender, Vehicle_Damage, Vehicle_Age
-#fig=plt.figure()
-#plt.rcParams['figure.figsize']=(20,7)
-#plt.subplot(1,3,1)
-#sns.kdeplot(x=df_cluster_AGE['Age'],hue=df_cluster_AGE['Gender'],palette="crest", multiple='stack')
-#plt.title('Age vs Gender on Top Cluster')
-#plt.subplot(1,3,2)
-#sns.kdeplot(x=df_cluster_AGE['Age'],hue=df_cluster_AGE['Vehicle_Damage'],palette="crest", multiple='stack')
-#plt.title('Age vs Vehicle_Damage on Top Cluster')
-#plt.subplot(1,3,3)
-#sns.kdeplot(x=df_cluster_AGE['Age'],hue=df_cluster_AGE['Vehicle_Age'],palette="crest", multiple='stack')
-#plt.title('Age vs Vehicle_Age on Top Cluster')
-#st.pyplot(fig)
+fig=plt.figure()
+plt.rcParams['figure.figsize']=(20,7)
+plt.subplot(1,3,1)
+sns.kdeplot(x=df_cluster_AGE['Age'],hue=df_cluster_AGE['Gender'],palette="crest", multiple='stack')
+plt.title('Age vs Gender on Top Cluster')
+plt.subplot(1,3,2)
+sns.kdeplot(x=df_cluster_AGE['Age'],hue=df_cluster_AGE['Vehicle_Damage'],palette="crest", multiple='stack')
+plt.title('Age vs Vehicle_Damage on Top Cluster')
+plt.subplot(1,3,3)
+sns.kdeplot(x=df_cluster_AGE['Age'],hue=df_cluster_AGE['Vehicle_Age'],palette="crest", multiple='stack')
+plt.title('Age vs Vehicle_Age on Top Cluster')
+st.pyplot(fig)
