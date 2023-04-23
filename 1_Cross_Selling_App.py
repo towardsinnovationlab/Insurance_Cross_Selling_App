e17281ca191464b1720a9d794b715c23579702c6
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-04-23 22:15:11 +0200

    Update 1_Cross_Selling_App.py

--- 1_Cross_Selling_App.py
+++ 1_Cross_Selling_App.py
@@ -8,3 +8,4 @@ st.markdown("""
         **👈 Select a page from the dropdown on the left** to see some applications of the app!"""
         )
 
+st.image("http://www.agentenews.com/wp-content/uploads/2013/09/Cross-Selling-Tips.jpg", width=500)
