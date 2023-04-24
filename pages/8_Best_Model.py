9b7c9e3473e012466e6ca9d6e0c55c62911f45f2
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-04-24 15:52:21 +0200

    Update 8_Best_Model.py

--- pages/8_Best_Model.py
+++ pages/8_Best_Model.py
@@ -59,10 +59,10 @@ predictions_tr_ = pd.DataFrame(predictions_tr_, columns=['Prediction']).reset_in
 predictions_te_ = pd.DataFrame(predictions_te_, columns=['Prediction']).reset_index(drop=True)
 predictions = pd.concat([predictions_tr_, predictions_te_], axis=0).reset_index(drop=True)
 df = pd.concat([df, predictions], axis=1)
-
+df.describe(type='all')
 fig=plt.figure()
 sns.countplot(x=df['Response'], data=df)
-sns.countplot(x=df['Prediction'], data=df)
+#sns.countplot(x=df['Prediction'], data=df)
 st.pyplot(fig)
 
 
