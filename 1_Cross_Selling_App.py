42385e56b2ce242d171b4123dc89f0926f3c9b69
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-05-23 07:05:46 +0200

    Update 1_Cross_Selling_App.py

--- 1_Cross_Selling_App.py
+++ 1_Cross_Selling_App.py
@@ -10,4 +10,6 @@ st.markdown("""
 
 st.image("./images/cross_selling_homepage.jpg", width=500)
 
-Read the article [](https://medium.com/@c.giancaterino/cross-selling-web-app-on-streamlit-cloud-205108d9b72b)
+url = 'https://medium.com/@c.giancaterino/cross-selling-web-app-on-streamlit-cloud-205108d9b72b'
+
+st.write("Read the article: webbrowser.open_new_tab(url)")
